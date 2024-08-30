d456461d3957bfe4f502ece1e8efeb6a4451c708
Santiago Fraire Willemoes <santiwilly@gmail.com>
2024-08-30 14:11:40 +0200

    feat: add CNAME

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import topLevelAwait from "vite-plugin-top-level-await";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/",
   plugins: [react(), wasm(), topLevelAwait()],
   resolve: {
     alias: {
