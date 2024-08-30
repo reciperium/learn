80135660a32a81bb05e525cc82efcc2b2c7b3f49
Santiago Fraire Willemoes <santiwilly@gmail.com>
2024-08-30 11:25:27 +0200

    ci: add release pipeline

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,23 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import wasm from "vite-plugin-wasm";
 import path from "path"
+import topLevelAwait from "vite-plugin-top-level-await";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), wasm()],
+  plugins: [react(), wasm(), topLevelAwait()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          codeMirror: ['@uiw/react-codemirror']
+        }
+      }
+    }
+  }
 })
