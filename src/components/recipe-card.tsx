9b5fed48059496be551f30de83facf1e67b788e8
Santiago Fraire Willemoes <santiwilly@gmail.com>
2024-10-04 10:32:41 +0200

    feat(card): add shadows and border color

--- src/components/recipe-card.tsx
+++ src/components/recipe-card.tsx
@@ -18,7 +18,7 @@ export default function RecipeCard({ parsedRecipe, rawRecipe }: RecipeProps) {
       .filter((v) => !!v) || [];
 
   return (
-    <div className="relative rounded-md border bg-muted p-6 ">
+    <div className="relative rounded-md border border-slate-400 dark:border-slate-500 bg-muted p-6 shadow-lg dark:shadow-white/40 antialiased">
       <div className="absolute top-0 right-0 pt-3 pr-3">
         <ShareButton recipe={rawRecipe} />
       </div>
