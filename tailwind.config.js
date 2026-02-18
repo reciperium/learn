823eefee102ba2cc982053472e0163b461a705d9
Santiago Fraire Willemoes <santiwilly@gmail.com>
2026-02-18 08:38:31 +0000

    feat(tailwind): migrate to v4

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   darkMode: ["class"],
   content: [
@@ -6,7 +7,7 @@ module.exports = {
     "./components/**/*.{ts,tsx}",
     "./app/**/*.{ts,tsx}",
     "./src/**/*.{ts,tsx}",
-    "./node_modules/@reciperium/**/*.{ts,tsx,js,jsx}",
+    "./node_modules/@reciperium",
   ],
   prefix: "",
   theme: {
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 import wasm from "vite-plugin-wasm";
-import path from "path"
+import path from "path";
 import topLevelAwait from "vite-plugin-top-level-await";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/",
-  plugins: [react(), wasm(), topLevelAwait()],
+  plugins: [tailwindcss(), react(), wasm(), topLevelAwait()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
@@ -17,9 +18,9 @@ export default defineConfig({
     rollupOptions: {
       output: {
         manualChunks: {
-          codeMirror: ['@uiw/react-codemirror']
-        }
-      }
-    }
-  }
-})
+          codeMirror: ["@uiw/react-codemirror"],
+        },
+      },
+    },
+  },
+});
