db9795047f4a102c2fa69345d6d09a98384858d4
Santiago Fraire Willemoes <santiwilly@gmail.com>
2026-03-02 08:57:34 +0000

    fix: bump dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -4,21 +4,35 @@ import wasm from "vite-plugin-wasm";
 import path from "path";
 import topLevelAwait from "vite-plugin-top-level-await";
 import tailwindcss from "@tailwindcss/vite";
+// import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/",
-  plugins: [tailwindcss(), react(), wasm(), topLevelAwait()],
+  plugins: [
+    // visualizer({ gzipSize: true, open: true }),
+    tailwindcss(),
+    react(),
+    wasm(),
+    topLevelAwait(),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
   build: {
+    sourcemap: false,
+
     rollupOptions: {
       output: {
         manualChunks: {
-          codeMirror: ["@uiw/react-codemirror"],
+          radix: ["@radix-ui/react-dropdown-menu", "@radix-ui/react-slot"],
+          codeMirror: ["@uiw/react-codemirror", "@uiw/codemirror-themes"],
+          reciperium: [
+            "@reciperium/react-recipe",
+            "@reciperium/recipe-parser-wasm",
+          ],
         },
       },
     },
